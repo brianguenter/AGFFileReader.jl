01b3b80e68ce44a230ebf0d15c95a7c9fb954b7c
brianguenter <1brianguenter@gmail.com>
2025-02-21 15:38:09 -0800

    added Temperature import from unitful in extension

--- ext/AGFFileReaderExtensions.jl
+++ ext/AGFFileReaderExtensions.jl
@@ -4,6 +4,7 @@ using GR
 using Plots
 using ForwardDiff: derivative
 using Unitful
+using Unitful: Temperature
 
 #NOTE: for some reason drawglassmap doesn't work correctly, get error saying ForwardDiff is not defined. Not sure where this error is coming from.
 
